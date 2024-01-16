c6ef601c8208b581e1b234bda68723ed41559844
oriemiobang <oriemiobango@gmail.com>
2024-01-16 10:45:55 +0300

    modified the buttons

---
+++ script.js
@@ -0,0 +1,13 @@
+function uptoDownredirectToLink(){
+    var upToDownButton = document.getElementById("uptodown");
+    var newTab = window.open('https://mandrillapp.com/track/click/31134266/dha-anywaa.en.uptodown.com?p=eyJzIjoiVndwNmFiSHdwZ0w5TU9lV3dDYlVFcHk2VmtNIiwidiI6MSwicCI6IntcInVcIjozMTEzNDI2NixcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2RoYS1hbnl3YWEuZW4udXB0b2Rvd24uY29tXFxcL2FuZHJvaWRcIixcImlkXCI6XCI5NTUzMjc4NWI2OTM0YWQwOGFhNTMwNWZhMjJmNWJkZVwiLFwidXJsX2lkc1wiOltcIjU5OTYxYjAxM2ExZTk4MzBlZjdjNzI0MDEyZjE5ZDdlMjE4ZDkwYzdcIl19In0', '_blank');
+    newTab.focus();
+}
+
+function playstoreredirectToLink() {
+// Get the information container element
+var infoContainer = document.getElementById("playstore");
+
+// Display the information container
+alert( 'The app is not availabe on playstore yet\n pls get it from Uptodwon.');
+}
